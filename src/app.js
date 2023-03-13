850c99acd4688bab619bd23c0ff4d0705a04ecd1
agustinazineira <agus2465@hotmail.com>
2023-03-13 20:27:48 -0300

    servidores web

--- src/app.js
+++ src/app.js
@@ -59,34 +59,3 @@ operacionesProductos()
 
 
 
-/*const manager = new ProductManager(`./files/Productos.json`);
-
-const operacionesProductos = async () => {
-    try {
-
-       await manager.getProducts();
-
-        let product1 = await manager.addProduct("PR01", "Philadelphia", "Salmon, queso philadelphia y palta", 2100, "Philadelphia.jpg", 50)
-        console.log(product1);
-
-        let product2 = await manager.addProduct("PR02", "Tamago", "Salmon, queso philadelphia y ciboullette", 2400, "Tamago.jpg", 25)
-        console.log(product2)
-        
-        let product3 = await manager.addProduct("PR03", "Soul", "Salmon y palmito, envuelto en queso", 2800, "Soul.jpg", 25)
-        console.log(product3)
-
-        let productoId= await manager.getProductById(2)
-         console.log(productoId);
-
-        let productAct = await manager.updateProduct(1, "Philadelphia");
-         console.log(productAct);
-
-        let deleteproduct1 = await manager.deleteProducts(3);
-        console.log(deleteproduct1)
-        
-    } catch (error) {
-        console.log(error);
-    }
-}
-operacionesProductos();
-*/
